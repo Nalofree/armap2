16720e1732f875027edc1a3bb28bac90e609b047
Nalofree <nalofree@gmail.com>
2017-02-22 13:17:33 +0800

    uplimage

--- public/js/addobject.js
+++ public/js/addobject.js
@@ -265,7 +265,13 @@ $(document).ready(function() {
         console.log(data);
         // $('.add-photos-items').empty();
         for (var i = 0; i < data.images.length; i++) {
-          $('.add-photos-items').append("<a href=/uploads/"+data.images[i].image_filename+">"+data.images[i].image_filename+"</a>");
+          $('<div class="add-photos-item"><img alt="" width=180><div class="photos-settings"><div class="radio"><label for="mainPhoto"><input type="radio" name="mainPhoto" value="'+data[i].image_id+'" data-title="'+data[i].image_id+'">Главная</label></div><a href="#" class="deluplimage"  data-title="'+data[i].image_id+'">Удалить</a></div></div>').load(function () {
+            $('.add-photos-items').append($(this));
+          }).error(function(){
+              // error
+              console.log('loading error');
+          }).attr('src','/uploads/'+data.images[i].image_filename);
+          // $('.add-photos-items').append("<a href=/uploads/"+data.images[i].image_filename+">"+data.images[i].image_filename+"</a>");
           // $('.add-photos-items').append("<img src=/uploads/"+data.images[i].image_filename+">");
 
           // $('.add-photos-items').append('<div class="add-photos-item"><img src="/uploads/'+data[i].image_filename+'" alt="" width=180><div class="photos-settings"><div class="radio"><label for="mainPhoto"><input type="radio" name="mainPhoto" value="'+data[i].image_id+'" data-title="'+data[i].image_id+'">Главная</label></div><a href="#" class="deluplimage"  data-title="'+data[i].image_id+'">Удалить</a></div></div>');
